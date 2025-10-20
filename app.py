a49eff85253d712186566b4657eae2c8f8dd090f
rineheaj <joshtrineheart@gmail.com>
2025-10-20 00:14:53 -0500

    less server surface

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def gen_token():
     display_name = request.json.get("displayName")
 
     if code != SECRET_CODE or not display_name:
-        return jsonify({"error": "Wrong code or missing chat name 😞"}), 401
+        return jsonify({"error": "Oops 😞"}), 401
 
     payload = {
         "user": display_name,
